4184f75534a32d00b66950538f4d46767112c5ac
sntgrazi <sntgrazi22@gmail.com>
2023-04-30 19:18:34 -0300

    teste

--- railway.ml
+++
@@ -1,9 +0,0 @@
-buildCommand: |
-  mkdir -p webroot
-  cp -R ./* webroot/
-  echo 'RewriteEngine On' > webroot/.htaccess
-  echo 'RewriteCond %{REQUEST_FILENAME} !-f' >> webroot/.htaccess
-  echo 'RewriteRule ^ index.php [QSA,L]' >> webroot/.htaccess
-
-staticDirs:
-  - webroot
