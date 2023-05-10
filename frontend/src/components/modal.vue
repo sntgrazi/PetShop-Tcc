51b26af116661d5b5211558f50529c245b51fb64
sntgrazi <sntgrazi22@gmail.com>
2023-05-10 00:58:56 -0300

    teste

--- frontend/src/components/modal.vue
+++ frontend/src/components/modal.vue
@@ -13,7 +13,6 @@
     <div class="modal-body">
       <form @submit.prevent="userId == false ? submitForm() : editarForm()">
         <div class="form-inputs" v-show="etapaAtual === 1">
-<<<<<<< HEAD
           <div class="inputCadastroCliente" v-if="mostrarInputsCadastro">
             <BaseInput :modelValue="cliente.nome" @update:modelValue="(newValue) =>
               (cliente.nome = newValue)" :label="'Nome'" :idInput="'inputName'" />
@@ -54,31 +53,14 @@
               <BaseInput :modelValue="animal.peso" @update:modelValue="(newValue) =>
                 (animal.peso = newValue)" :label="'Peso'" :idInput="'inputPeso'" />
             </div>
-=======
-          <BaseInput :modelValue="cliente.nome" @update:modelValue="(newValue) => (cliente.nome = newValue)
-            " :label="'Nome'" v-if="mostrarInputsCadastro" :idInput="'inputName'" />
-
-          <div class="colunaForm">
-            <BaseInput :modelValue="cliente.cpf" @update:modelValue="(newValue) => (cliente.cpf = newValue)"
-              :label="'Cpf'" v-if="mostrarInputsCadastro" :idInput="'inputCpf'" />
-            <BaseInput :modelValue="cliente.rg" @update:modelValue="(newValue) => (cliente.rg = newValue)" :label="'Rg'"
-              v-if="mostrarInputsCadastro" :idInput="'inputRg'" />
-          </div>
-
-          <div class="colunaForm">
-            <BaseInput :modelValue="cliente.telefone" @update:modelValue="(newValue) => (cliente.telefone = newValue)"
-              :label="'Telefone'" v-if="mostrarInputsCadastro" :idInput="'inputTelefone'" />
-            <BaseInput :modelValue="cliente.email" @update:modelValue="(newValue) => (cliente.email = newValue)"
-              :label="'Email'" v-if="mostrarInputsCadastro" :idInput="'inputEmail'" />
->>>>>>> origin/Style
           </div>
 
           <div class="modal-footer">
             <button type="button" class="proxima-etapa" @click="etapaAtual = 2">Próximo</button>
           </div>
         </div>
+
         <div class="form-inputs" v-show="etapaAtual === 2">
-<<<<<<< HEAD
           <div class="inputCadastroCliente" v-if="mostrarInputsCadastro">
             <div class="colunaForm">
               <BaseInput :modelValue="cliente.cep" @update:modelValue="(newValue) =>
@@ -138,47 +120,16 @@
               <BaseInput :modelValue="animal.porte" @update:modelValue="(newValue) =>
                 (animal.porte = newValue)" :label="'Porte'" :idInput="'inputPorte'" />
             </div>
-=======
-          <div class="colunaForm">
-            <BaseInput :modelValue="cliente.cep" @update:modelValue="(newValue) => (cliente.cep = newValue)"
-              :label="'Cep'" v-if="mostrarInputsCadastro" :idInput="'inputCep'" />
-              <button type="button" class="btn-pesquisar" @click="procurarEndereço"><i class="fa-solid fa-magnifying-glass"></i></button>
-              
-            <BaseInput :modelValue="cliente.bairro" @update:modelValue="(newValue) => (cliente.bairro = newValue)"
-              :label="'Bairro'" v-if="mostrarInputsCadastro" :idInput="'inputBairro'" />
-          </div>
-          <div class="colunaForm">
-            <BaseInput :modelValue="cliente.rua" @update:modelValue="(newValue) => (cliente.rua = newValue)"
-              :label="'Rua'" v-if="mostrarInputsCadastro" :idInput="'inputRua'" />
-          </div>
-
-          <div class="colunaForm">
-            <BaseInput :modelValue="cliente.cidade" @update:modelValue="(newValue) => (cliente.cidade = newValue)"
-              :label="'Cidade'" v-if="mostrarInputsCadastro" :idInput="'inputCidade'" />
-            <BaseInput :modelValue="cliente.uf" @update:modelValue="(newValue) => (cliente.uf = newValue)" :label="'Uf'"
-              v-if="mostrarInputsCadastro" :idInput="'inputUf'" />
-            <BaseInput :modelValue="cliente.n_casa" @update:modelValue="(newValue) => (cliente.n_casa = newValue)"
-              :label="'N°'" v-if="mostrarInputsCadastro" :idInput="'inputN_Casa'" />
->>>>>>> origin/Style
           </div>
 
           <div class="modal-footer">
             <button type="button" class="proxima-etapa" @click="etapaAtual = 1">
-<<<<<<< HEAD
               <i class="fa-solid fa-arrow-left"></i> Voltar
-=======
-              <i class="fa-solid fa-arrow-left"></i>
-              Voltar
->>>>>>> origin/Style
             </button>
 
             <button class="confirm">{{ botaoConfirm }}</button>
           </div>
         </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/Style
       </form>
     </div>
   </div>
@@ -187,12 +138,9 @@
 <script>
 import ApiController from "@/ApiController";
 import BaseInput from "./BaseInput.vue";
-<<<<<<< HEAD
 import Swal from "sweetalert2";
 import axios from "axios";
 
-=======
->>>>>>> origin/Style
 export default {
   name: "Modal",
   emits: ["atualizarTabela"],
@@ -202,6 +150,7 @@ export default {
     "icon",
     "mostrarInputsCadastro",
     "mostrarInputsAgendamento",
+    "inputsAnimais",
     "toggle",
     "active",
     "getClientes"
@@ -213,11 +162,7 @@ export default {
     return {
       // Cliente DATA
       etapaAtual: 1,
-<<<<<<< HEAD
       cliente: {},
-=======
-      cliente: { },
->>>>>>> origin/Style
       titulo:
         this.tipo === "cliente"
           ? "Cadastrar Cliente"
@@ -238,10 +183,7 @@ export default {
       if (this.tipo == "cliente") {
         ApiController.cadastrarCliente(this.cliente)
           .then(() => {
-<<<<<<< HEAD
             Swal.fire("", "Cliente cadastrado com sucesso!", "success");
-=======
->>>>>>> origin/Style
             this.$emit("atualizarTabela");
             this.toggle();
             this.cliente = {};
@@ -268,10 +210,7 @@ export default {
      if(this.tipo == "cliente"){
       ApiController.editarCliente(this.userId, this.cliente)
         .then(() => {
-<<<<<<< HEAD
           Swal.fire("", "Cliente atualizado com sucesso!", "success");
-=======
->>>>>>> origin/Style
           this.$emit("atualizarTabela");
           this.toggle();
           this.cliente = {};
@@ -279,7 +218,6 @@ export default {
         .catch(error => {
           console.log(error);
         });
-<<<<<<< HEAD
      } else if (this.tipo == "agenda"){
       console.log("agendamento")
      } else if (this.tipo == "Pets"){
@@ -289,31 +227,12 @@ export default {
           this.$emit("atualizarTabela");
           this.toggle();
           this.cliente = {};
-=======
-    },
-    async Cliente() {
-      ApiController.cliente(this.userId)
-        .then((item) => {
-            this.cliente = {
-              bairro: item.endereco
-            };
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    async Cliente() {
-      ApiController.cliente(this.userId)
-        .then((cliente) => {
-          this.cliente = cliente;
->>>>>>> origin/Style
         })
         .catch(error => {
           console.log(error);
         });
      }
     },
-<<<<<<< HEAD
 
     async buscar() {
       if(this.tipo == "cliente"){
@@ -391,11 +310,12 @@ export default {
   transform: translate(-50%, -50%);
   width: 500px;
   max-width: 90%;
-  background-color: #02396f ;
+  background-color: #4d72d6;
   padding: 1.2rem;
   border-radius: 0.5rem;
   z-index: 10;
 }
+
 .form-overlay {
   position: fixed;
   top: 0;
@@ -405,6 +325,7 @@ export default {
   background-color: rgba(0, 0, 0, 0.6);
   z-index: 5;
 }
+
 .modal-header {
   display: flex;
   justify-content: space-between;
@@ -414,9 +335,11 @@ export default {
   padding-bottom: 1rem;
   color: #ffffff;
 }
+
 .modal-header i {
   font-size: 30px;
 }
+
 .titulo-modal {
   display: flex;
   flex-direction: row;
@@ -424,19 +347,24 @@ export default {
   justify-content: center;
   gap: 20px;
 }
+
 form {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
+
 .form-inputs {
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 5px;
 }
+
 .form-inputs label {
   color: white;
+  margin-top: 10px;
 }
+
 .form-inputs input {
   width: 280px;
   height: 35px;
@@ -447,10 +375,7 @@ form {
   font-size: 1.2em;
   background-color: #fff;
 }
-.form-inputs input[type="date"],
-.form-inputs input[type="time"] {
-  width: 220px;
-}
+
 .colunaForm {
   display: flex;
   flex-direction: row;
@@ -458,20 +383,16 @@ form {
   justify-content: center;
   gap: 20px;
 }
-<<<<<<< HEAD
 
 #inputName,
 #inputTutor,
 #inputEmail {
-=======
-#inputName {
->>>>>>> origin/Style
   width: 440px;
 }
+
 .colunaForm #inputCpf,
 #inputRg,
 #inputTelefone,
-<<<<<<< HEAD
 #inputCep,
 #inputPet,
 #inputAltura,
@@ -480,26 +401,25 @@ form {
 #inputPelagem,
 #inputPorte,
 #inputRaca {
-=======
-#inputEmail,
-#inputCep {
->>>>>>> origin/Style
   width: 210px;
 }
+
 .colunaForm #inputBairro {
   width: 170px;
 }
+
 .colunaForm #inputUf,
 .colunaForm #inputN_Casa {
   width: 70px;
 }
+
 .colunaForm #inputRua {
   width: 400px;
 }
+
 .colunaForm #inputCidade {
   width: 220px;
 }
-<<<<<<< HEAD
 
 .colunaForm #inputDataNascimento {
   width: 140px;
@@ -526,37 +446,30 @@ form {
   gap: 5px;
 }
 
-=======
->>>>>>> origin/Style
 .modal-footer {
   margin-top: 1rem;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
 }
+
 .modal-footer button {
-  background-color: #e2ebf4;
+  background-color: #010d55;
   padding: 0.6rem 1.2rem;
-  color: #02396f;
+  color: #fff;
   border: none;
-  font: bold;
   border-radius: 0.25rem;
   cursor: pointer;
   opacity: 0.9;
-  font-size: 1.2rem;
+  font-size: 1rem;
 }
+
 .modal-footer button i {
   margin-right: 5px;
 }
-.modal-footer button:hover {
-  transition: 1s;
-  background-color: #0005;
-  color: white;
-  
-}
+
 .modal-footer .confirm {
-  background-color: #02396f;
-  color:white;
+  background-color: #010d55;
 }
 
 .close {
@@ -565,28 +478,29 @@ form {
   cursor: pointer;
   color: #fff;
 }
-.close:hover{
-  color:#0005;
-  transition: 1s;
-}
+
 #inputCep {
-  padding-right: 40px; /* Espaçamento para o botão */
+  padding-right: 40px;
+  /* Espaçamento para o botão */
 }
+
 .btn-pesquisar {
   position: absolute;
   background-color: transparent;
   border: none;
   font-size: 1em;
   cursor: pointer;
-  margin-top: 30px;
+  margin-top: 35px;
   margin-right: 20px;
 }
+
 @media screen and (max-width: 750px) {
   .colunaForm {
     display: flex;
     flex-direction: column;
     gap: 10px;
   }
+
   .colunaForm #inputCpf,
   .colunaForm #inputRg,
   .colunaForm #inputTelefone,
@@ -597,7 +511,6 @@ form {
   .colunaForm #inputUf,
   .colunaForm #inputBairro,
   .colunaForm #inputRua,
-<<<<<<< HEAD
   .colunaForm #inputN_Casa,
   #inputTutor,
   .colunaForm #inputPet,
@@ -609,28 +522,30 @@ form {
   .colunaForm #inputPelagem,
   .colunaForm #inputRaca,
   .colunaForm #inputPorte, .select-breed, .select-species {
-=======
-  .colunaForm #inputN_Casa {
->>>>>>> origin/Style
     width: 300px;
   }
+
   .modal-footer {
     gap: 20px;
   }
+
   #inputCep {
-  padding-right: 40px; /* Espaçamento para o botão */
+    padding-right: 40px;
+    /* Espaçamento para o botão */
+  }
+
+  .btn-pesquisar {
+    position: absolute;
+    background-color: transparent;
+    border: none;
+    font-size: 1em;
+    cursor: pointer;
+    margin-bottom: 75px;
+    margin-left: 260px;
+    padding: 10px;
+  }
 }
-.btn-pesquisar {
-  position: absolute;
-  background-color: transparent;
-  border: none;
-  font-size: 1em;
-  cursor: pointer;
-  margin-bottom: 75px;
-  margin-left: 260px;
-  padding: 10px;
-}
-}
+
 @media screen and (max-width: 350px) {
   #modal {
     position: absolute;
