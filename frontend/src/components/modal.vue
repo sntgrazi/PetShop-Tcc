9b2682e2c3c7d7a296873ea0bb1dc0fc57f70def
sntgrazi <sntgrazi22@gmail.com>
2023-05-13 13:44:30 -0300

    organizando arq

--- frontend/src/components/modal.vue
+++ frontend/src/components/modal.vue
@@ -14,40 +14,20 @@
       <form @submit.prevent="userId == false ? submitForm() : editarForm()">
         <div class="form-inputs" v-show="etapaAtual === 1">
           <div class="inputCadastroCliente" v-if="mostrarInputsCadastro">
-            <BaseInput
-              :modelValue="cliente.nome"
-              @update:modelValue="(newValue) =>
-              (cliente.nome = newValue)"
-              :label="'Nome'"
-              :idInput="'inputName'"
-            />
+            <BaseInput :modelValue="cliente.nome" @update:modelValue="(newValue) =>
+              (cliente.nome = newValue)" :label="'Nome'" :idInput="'inputName'" />
 
             <div class="colunaForm">
-              <BaseInput
-                :modelValue="cliente.cpf"
-                @update:modelValue="(newValue) =>
-                (cliente.cpf = newValue)"
-                :label="'Cpf'"
-                :idInput="'inputCpf'"
-              />
+              <BaseInput :modelValue="cliente.cpf" @update:modelValue="(newValue) =>
+                (cliente.cpf = newValue)" :label="'Cpf'" :idInput="'inputCpf'" />
 
-              <BaseInput
-                :modelValue="cliente.telefone"
-                @update:modelValue="(newValue) =>
-                (cliente.telefone = newValue)"
-                :label="'Telefone'"
-                :idInput="'inputTelefone'"
-              />
+              <BaseInput :modelValue="cliente.telefone" @update:modelValue="(newValue) =>
+                (cliente.telefone = newValue)" :label="'Telefone'" :idInput="'inputTelefone'" />
             </div>
 
             <div class="colunaForm">
-              <BaseInput
-                :modelValue="cliente.email"
-                @update:modelValue="(newValue) =>
-                (cliente.email = newValue)"
-                :label="'Email'"
-                :idInput="'inputEmail'"
-              />
+              <BaseInput :modelValue="cliente.email" @update:modelValue="(newValue) =>
+                (cliente.email = newValue)" :label="'Email'" :idInput="'inputEmail'" />
             </div>
           </div>
 
@@ -55,47 +35,22 @@
             <BaseInput :label="'Tutor'" :idInput="'inputTutor'" />
 
             <div class="colunaForm">
-              <BaseInput
-                :modelValue="animal.nome_pet"
-                @update:modelValue="(newValue) =>
-                (animal.nome_pet = newValue)"
-                :label="'Pet'"
-                :idInput="'inputPet'"
-              />
+              <BaseInput :modelValue="animal.nome_pet" @update:modelValue="(newValue) =>
+                (animal.nome_pet = newValue)" :label="'Pet'" :idInput="'inputPet'" />
 
-              <BaseInput
-                :modelValue="animal.data_nascimento"
-                @update:modelValue="(newValue) =>
-                (animal.data_nascimento = newValue)"
-                :label="'Nascimento'"
-                :idInput="'inputDataNascimento'"
-              />
+              <BaseInput :modelValue="animal.data_nascimento" @update:modelValue="(newValue) =>
+                (animal.data_nascimento = newValue)" :label="'Nascimento'" :idInput="'inputDataNascimento'" />
 
-              <BaseInput
-                :modelValue="animal.sexo"
-                @update:modelValue="(newValue) =>
-                (animal.sexo = newValue)"
-                :label="'Sexo'"
-                :idInput="'inputSexo'"
-              />
+              <BaseInput :modelValue="animal.sexo" @update:modelValue="(newValue) =>
+                (animal.sexo = newValue)" :label="'Sexo'" :idInput="'inputSexo'" />
             </div>
 
             <div class="colunaForm">
-              <BaseInput
-                :modelValue="animal.altura"
-                @update:modelValue="(newValue) =>
-                (animal.altura = newValue)"
-                :label="'Altura'"
-                :idInput="'inputAltura'"
-              />
+              <BaseInput :modelValue="animal.altura" @update:modelValue="(newValue) =>
+                (animal.altura = newValue)" :label="'Altura'" :idInput="'inputAltura'" />
 
-              <BaseInput
-                :modelValue="animal.peso"
-                @update:modelValue="(newValue) =>
-                (animal.peso = newValue)"
-                :label="'Peso'"
-                :idInput="'inputPeso'"
-              />
+              <BaseInput :modelValue="animal.peso" @update:modelValue="(newValue) =>
+                (animal.peso = newValue)" :label="'Peso'" :idInput="'inputPeso'" />
             </div>
           </div>
 
@@ -107,61 +62,31 @@
         <div class="form-inputs" v-show="etapaAtual === 2">
           <div class="inputCadastroCliente" v-if="mostrarInputsCadastro">
             <div class="colunaForm">
-              <BaseInput
-                :modelValue="cliente.cep"
-                @update:modelValue="(newValue) =>
-                (cliente.cep = newValue)"
-                :label="'Cep'"
-                :idInput="'inputCep'"
-              />
+              <BaseInput :modelValue="cliente.cep" @update:modelValue="(newValue) =>
+                (cliente.cep = newValue)" :label="'Cep'" :idInput="'inputCep'" />
 
               <button type="button" class="btn-pesquisar" @click="procurarEndereço">
                 <i class="fa-solid fa-magnifying-glass"></i>
               </button>
 
-              <BaseInput
-                :modelValue="cliente.bairro"
-                @update:modelValue="(newValue) =>
-                (cliente.bairro = newValue)"
-                :label="'Bairro'"
-                :idInput="'inputBairro'"
-              />
+              <BaseInput :modelValue="cliente.bairro" @update:modelValue="(newValue) =>
+                (cliente.bairro = newValue)" :label="'Bairro'" :idInput="'inputBairro'" />
             </div>
 
             <div class="colunaForm">
-              <BaseInput
-                :modelValue="cliente.rua"
-                @update:modelValue="(newValue) =>
-                (cliente.rua = newValue)"
-                :label="'Rua'"
-                :idInput="'inputRua'"
-              />
+              <BaseInput :modelValue="cliente.rua" @update:modelValue="(newValue) =>
+                (cliente.rua = newValue)" :label="'Rua'" :idInput="'inputRua'" />
             </div>
 
             <div class="colunaForm">
-              <BaseInput
-                :modelValue="cliente.cidade"
-                @update:modelValue="(newValue) =>
-                (cliente.cidade = newValue)"
-                :label="'Cidade'"
-                :idInput="'inputCidade'"
-              />
+              <BaseInput :modelValue="cliente.cidade" @update:modelValue="(newValue) =>
+                (cliente.cidade = newValue)" :label="'Cidade'" :idInput="'inputCidade'" />
 
-              <BaseInput
-                :modelValue="cliente.uf"
-                @update:modelValue="(newValue) =>
-                (cliente.uf = newValue)"
-                :label="'Uf'"
-                :idInput="'inputUf'"
-              />
+              <BaseInput :modelValue="cliente.uf" @update:modelValue="(newValue) =>
+                (cliente.uf = newValue)" :label="'Uf'" :idInput="'inputUf'" />
 
-              <BaseInput
-                :modelValue="cliente.n_casa"
-                @update:modelValue="(newValue) =>
-                (cliente.n_casa = newValue)"
-                :label="'N°'"
-                :idInput="'inputN_Casa'"
-              />
+              <BaseInput :modelValue="cliente.n_casa" @update:modelValue="(newValue) =>
+                (cliente.n_casa = newValue)" :label="'N°'" :idInput="'inputN_Casa'" />
             </div>
           </div>
 
@@ -170,37 +95,25 @@
               <div class="selectCampo">
                 <label for="especie">Espécie</label>
                 <select v-model="animal.especie" id="select-especie">
-                  <option value selected>Selecione a espécie</option>
-                  <option value="dog" data-nome="Cachorro">Cachorro</option>
-                  <option value="cat">Gato</option>
+                  <option v-for="especie in especies" :value="especie.id">{{ especie.nome }}</option>
                 </select>
               </div>
 
               <div class="selectCampo">
                 <label for="raca">Raça</label>
                 <select v-model="animal.raca" id="select-raca">
-                  <option value>Selecione a raça</option>
-                  <option v-for="breed in breeds" :value="breed.name">{{ breed.name }}</option>
+                  <option v-for="breed in breeds" :value="breed.name">{{ breed.name
+                  }}</option>
                 </select>
               </div>
             </div>
 
             <div class="colunaForm">
-              <BaseInput
-                :modelValue="animal.pelagem"
-                @update:modelValue="(newValue) =>
-                (animal.pelagem = newValue)"
-                :label="'Pelagem'"
-                :idInput="'inputPelagem'"
-              />
+              <BaseInput :modelValue="animal.pelagem" @update:modelValue="(newValue) =>
+                (animal.pelagem = newValue)" :label="'Pelagem'" :idInput="'inputPelagem'" />
 
-              <BaseInput
-                :modelValue="animal.porte"
-                @update:modelValue="(newValue) =>
-                (animal.porte = newValue)"
-                :label="'Porte'"
-                :idInput="'inputPorte'"
-              />
+              <BaseInput :modelValue="animal.porte" @update:modelValue="(newValue) =>
+                (animal.porte = newValue)" :label="'Porte'" :idInput="'inputPorte'" />
             </div>
           </div>
 
@@ -252,16 +165,18 @@ export default {
         this.tipo === "cliente"
           ? "Cadastrar Cliente"
           : this.tipo == "Pets"
-          ? "Novo Pet"
-          : "Novo Agendamento",
+            ? "Novo Pet"
+            : "Novo Agendamento",
       botaoConfirm:
         this.tipo === "cliente" || this.tipo === "Pets"
           ? "Cadastrar"
           : "Agendar",
       // Animais DATA
+      especies: [
+        { id: 'dog', nome: 'Cachorro' },
+        { id: 'cat', nome: 'Gato' }
+      ],
       breeds: [],
-      especieSelecionada: "",
-      racaSelecionada: "",
       animal: {}
     };
   },
@@ -354,22 +269,22 @@ export default {
   mounted() {
     if (this.userId != false) {
       (this.titulo = "Editar Cliente"), (this.botaoConfirm = "Editar");
+
       this.buscar();
     }
 
-    // Inicializa os selects com o Select2
+
     $("#select-especie").select2({
-      placeholder: "Selecione uma Espécie"
+      placeholder: "Selecione uma Espécie",
     });
     $("#select-raca").select2({
-      placeholder: "Selecione uma Raça"
+      placeholder: "Selecione uma Raça",
     });
 
     // Adiciona um listener de eventos para o select de espécies
     $("#select-especie").on("change", async e => {
       // Obtém a espécie selecionada
       this.especie = e.target.value;
-      this.animal.especie = $("#select-especie option:selected").data("nome");
 
       // Busca as raças correspondentes na API The Dog API
       const response = await axios.get(
@@ -391,15 +306,20 @@ export default {
           })
         );
       });
-
-      // Atualiza o select de raças com as novas opções
-      $("#select-raca").trigger("change");
-
-      $("#select-raca").on("change", e => {
-        // Obtém a raça selecionada
-        this.animal.raca = $("#select-raca option:selected").data("nome");
-      });
     });
+  },
+  watch: {
+    "animal.especie": async function (newSpecies) {
+      try {
+        const response = await fetch(
+          `https://api.the${newSpecies}api.com/v1/breeds`
+        );
+        const data = await response.json();
+        this.breeds = data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
   }
 };
 </script>
@@ -536,6 +456,19 @@ form {
   flex-direction: column;
   gap: 5px;
 }
+
+#select-especie {
+  width: 210px;
+  height: 35px;
+  border: none;
+  border-radius: 10px;
+  font-size: 17px;
+  outline: none;
+  box-shadow: 0 0.4rem 0.8rem #0005;
+  text-align: justify;
+  color: #000;
+}
+
 .select2-container .select2-selection {
   width: 210px;
   height: 35px;
