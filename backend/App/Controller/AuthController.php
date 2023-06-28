26189ddf3bc8234f695da139a3d1f739ccc1639b
sntgrazi <sntgrazi22@gmail.com>
2023-06-28 01:15:47 -0300

    teste

--- backend/App/Controller/AuthController.php
+++ backend/App/Controller/AuthController.php
@@ -10,42 +10,50 @@ use Psr\Http\Message\RequestInterface as Request;
 
 final class AuthController{
 
-    public function login(Request $request, Response $response,  $args){
+    public function login(Request $request, Response $response, $args)
+    {
         $data = $request->getParsedBody();
-  
+    
         $userDAO = new UserDAO();
         $userModel = new UserModel();
-        $userModel-> setEmail($data['email'])
-                  -> setSenha($data['senha']);
-
+        $userModel->setEmail($data['email'])
+                  ->setSenha($data['senha']);
+    
         $user = $userDAO->getUserByEmail($userModel);
-        if(!$user){
+        if (!$user) {
             $response = $response->withStatus(401);
             $response->getBody()->write(json_encode(['error' => 'Invalid credentials']));
             return $response->withHeader('Content-Type', 'application/json');
         }
-
-        if(!password_verify($userModel->getSenha(), $user['senha'])){
+    
+        // Define a senha recuperada no objeto $userModel
+        $userModel->setSenha($user['senha']);
+    
+        var_dump($userModel->getSenha());
+        var_dump($user['senha']);
+    
+        if (password_verify($userModel->getSenha(), $user['senha'])) {
             $response = $response->withStatus(401);
-            $response->getBody()->write(json_encode(['error' => 'Invalid credentials']));
+            $response->getBody()->write(json_encode(['error' => 'Senha inválida']));
             return $response->withHeader('Content-Type', 'application/json');
         }
-
+    
         $tokenPayLoad = [
             'email' => $user['email']
         ];
-
+    
         $secretKey = 'e#CQ9j1e@k3p$S0cT5uH*oG8g!zL';
         $algorithm = 'HS256';
-
+    
         $token = \Firebase\JWT\JWT::encode($tokenPayLoad, $secretKey, $algorithm);
-
+    
         $response = $response->withStatus(200);
         $response = $response->withHeader('Content-Type', 'application/json');
         $response->getBody()->write(json_encode(['token' => $token]));
-
+    
         return $response;
     }
+    
 
     public function verificaSenha(Request $request, Response $response, $args) {
         $data = $request->getParsedBody();
@@ -73,20 +81,17 @@ final class AuthController{
     }
 
     public function inserirUser(Request $request, Response $response, $args)
-    {
-        $data = $request->getParsedBody();
-        $userDAO = new UserDAO();
-        $userModel = new UserModel();
-        $userModel->setEmail($data['email'])
-            ->setSenha(password_hash($data['senha'], PASSWORD_DEFAULT)); 
+{
+    $data = $request->getParsedBody();
+    $userDAO = new UserDAO();
+    $userModel = new UserModel();
+    $userModel->setEmail($data['email'])
+        ->setSenha(password_hash($data['senha'], PASSWORD_DEFAULT));
 
-        $userDAO->inserirUser($userModel);
+    $userDAO->inserirUser($userModel);
 
-        $response->getBody()->write(json_encode(['message' => 'Usuário cadastrado com sucesso']));
-        return  $response->withHeader('Content-Type', 'application/json');
-    }
-
-    
-    
+    $response->getBody()->write(json_encode(['message' => 'Usuário cadastrado com sucesso']));
+    return  $response->withHeader('Content-Type', 'application/json');
+}
 
 }
